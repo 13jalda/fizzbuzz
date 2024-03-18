71c730472b38450d3144470dbdfe0edba39040bc
13jalda <j.aldacarro@gmail.com>
2024-03-18 10:40:38 +0100

    first

---
+++ fizzbuzz.py
@@ -0,0 +1,19 @@
+"""
+1
+2
+Fizz
+4
+Buzz
+"""
+def fizzbuzz(max_num):
+  for num in range(1, max_num + 1):
+    if num % 3 == 0 and num % 5 == 0:
+      print('FizzBuzz')
+    elif num % 3 == 0:
+      print('Fizz')
+    elif num % 5 == 0:
+      print('Buzz')
+    else:
+      print(num)
+
+fizzbuzz(105)
